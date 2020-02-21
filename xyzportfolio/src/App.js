12c4a03ec0e5896885ca5bfd876ec709fc6b955b
architekespy <architekespy@Reginalds-MacBook-Pro.local>
2020-02-21 15:41:14 -0500

    Footer completed

--- xyzportfolio/src/App.js
+++ xyzportfolio/src/App.js
@@ -5,6 +5,8 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import './App.css';
 
+import Footer from './components/Footer';
+
 class App extends React.Component {
 
   constructor(props) {
@@ -40,18 +42,22 @@ class App extends React.Component {
     return (
     <Router>
       <Container className="p-0" fluid={true}>
-        <Navbar className="border-bottom">
+        {/* expand="lg" to switch to toggle whenever screen size changes */}
+        <Navbar className="border-bottom" bg="transparent" expand="lg">
           <Navbar.Brand>architekxyz</Navbar.Brand>
           
-          <Navbar.Toggle aria-controls="navbar-toggle"/>
+          <Navbar.Toggle className="border-0" aria-controls="navbar-toggle"/>
           <Navbar.Collapse id="navbar-toggle">
-            <Nav>
+            {/* className="ml-auto" Links float to the right rather than to the left */}
+            <Nav className="ml-auto">
               <Link className="nav-link" to="/">Home</Link>
               <Link className="nav-link" to="/about">About</Link>
               <Link className="nav-link" to="/contact">Contact</Link>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
+
+        <Footer />
       </Container>
     </Router>
       );
