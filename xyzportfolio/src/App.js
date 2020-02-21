ab40078f6932f043003834c9d7de56a26b9c09ab
architekespy <architekespy@Reginalds-MacBook-Pro.local>
2020-02-21 11:39:52 -0500

    added a container

--- xyzportfolio/src/App.js
+++ xyzportfolio/src/App.js
@@ -1,11 +1,46 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
 import './App.css';
 
 class App extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'architekxyz',
+      //Everything we need for the navbar
+      headerLinks: [
+        { title: 'Home', path: '/'},
+        { title: 'About', path: '/about'},
+        { title: 'Contact', path: '/contact'}
+      ],
+
+      home: {
+        title: 'The Gallery',
+        subtitle: 'Photography by me',
+        text: 'Check me out',
+      },
+
+      about: {
+        title: 'About Me',
+        
+      },
+
+      contact: {
+        title: 'Let\'s Talk',
+        
+      }
+    }
+  }
+
   render() {
     return (
-    <div>Hello From React!</div>
+    <Router>
+      <Container className="p-0" fluid={true}>
+        <p>Hi from React</p>
+      </Container>
+    </Router>
       );
   }
   
