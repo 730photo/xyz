0a865edf9da195000abeca49563306e63515330b
architekespy <architekespy@Reginalds-MacBook-Pro.local>
2020-02-21 13:17:39 -0500

    Navbar complete

--- xyzportfolio/src/App.js
+++ xyzportfolio/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import './App.css';
 
 class App extends React.Component {
@@ -38,7 +40,18 @@ class App extends React.Component {
     return (
     <Router>
       <Container className="p-0" fluid={true}>
-        <p>Hi from React</p>
+        <Navbar className="border-bottom">
+          <Navbar.Brand>architekxyz</Navbar.Brand>
+          
+          <Navbar.Toggle aria-controls="navbar-toggle"/>
+          <Navbar.Collapse id="navbar-toggle">
+            <Nav>
+              <Link className="nav-link" to="/">Home</Link>
+              <Link className="nav-link" to="/about">About</Link>
+              <Link className="nav-link" to="/contact">Contact</Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
       </Container>
     </Router>
       );
